9bf37ad3f4e7585ebd913cd4b29a0701bb69ce9f
Tyler Sell <tylerselldesign@gmail.com>
2019-02-08 22:59:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i
     @phrase = params[:phrase].to_s
-    @array = []
+    array = []
     @number.times do |words|
-      "#{@phrase}"
+      array << @phrase
     end
   end
   
