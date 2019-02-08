48bd271a464212d470d454cdb5d7a8105d45171d
Tyler Sell <tylerselldesign@gmail.com>
2019-02-08 22:00:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ class App < Sinatra::Base
   # Write your code here!
   
   get '/reversename/:name' do 
-    @user_name = params[:name]
+    @user_name = params[:name].reverse 
     "#{@user_name}"
+  end
+  
+  get '/square/:number' do 
+    @number = params[:number].to_i 
+    "#{@number * @number}"
+  end
 
 end
